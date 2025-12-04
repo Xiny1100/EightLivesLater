bee72fadd796643a946774e943d89cf56ae39c16
Xiny1100 <lokexy-wm23@student.tarc.edu.my>
2025-12-04 23:50:24 +0800

    Sorting data + add effect
    organize materials, add the fire and smoke effect, add the rubbish and tank

---
+++ Assets/Kitchen/Free Wood Door Pack/Script/Door.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace DoorScript
+{
+    [RequireComponent(typeof(AudioSource))]
+
+
+    public class Door : MonoBehaviour
+    {
+        public bool open;
+        public float smooth = 1.0f;
+        float DoorOpenAngle = -90.0f;
+        float DoorCloseAngle = 0.0f;
+        public AudioSource asource;
+        public AudioClip openDoor, closeDoor;
+
+        private Quaternion targetRotation;
+        // Use this for initialization
+        void Start()
+        {
+            asource = GetComponent<AudioSource>();
+            targetRotation = Quaternion.Euler(0, DoorCloseAngle, 0);
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (open)
+            {
+                var target = Quaternion.Euler(0, DoorOpenAngle, 0);
+                transform.localRotation = Quaternion.Slerp(transform.localRotation, target, Time.deltaTime * 5 * smooth);
+
+            }
+            else
+            {
+                var target1 = Quaternion.Euler(0, DoorCloseAngle, 0);
+                transform.localRotation = Quaternion.Slerp(transform.localRotation, target1, Time.deltaTime * 5 * smooth);
+
+            }
+        }
+
+        public void OpenDoor()
+        {
+            open = !open;
+            asource.clip = open ? openDoor : closeDoor;
+            asource.Play();
+        }
+
+        // 🔥 Auto open when player enters
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                open = true;
+                targetRotation = Quaternion.Euler(0, DoorOpenAngle, 0);
+
+                if (openDoor != null && !asource.isPlaying)
+                {
+                    asource.PlayOneShot(openDoor);
+                }
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                open = false;
+
+                targetRotation = Quaternion.Euler(0, DoorCloseAngle, 0);
+                if (closeDoor != null && !asource.isPlaying)
+                    asource.PlayOneShot(closeDoor);
+                {
+                }
+            }
+        }
+    }
+}
